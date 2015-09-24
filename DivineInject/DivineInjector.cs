89448586704edb712fac928bff198d2bff6756eb
David Green <dave@activelylazy.co.uk>
2015-09-24 23:05:05 +0200

    Initial commit

---
+++ DivineInject/DivineInjector.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DivineInject
+{
+    public interface IBindingBuilder<TInterface>
+    {
+        DivineInjector To<TImpl>();
+    }
+
+    public class DivineInjector
+    {
+        private IDictionary<Type, Type> m_bindings = new Dictionary<Type, Type>();
+
+        public IBindingBuilder<T> Bind<T>()
+        {
+            return new BindingBuilder<T>(this);
+        }
+
+        public T Get<T>()
+        {
+            Type implType;
+            if (!m_bindings.TryGetValue(typeof(T), out implType))
+                return default(T);
+            return (T) Activator.CreateInstance(implType);
+        }
+
+        private void AddBinding<TInterface, TImpl>()
+        {
+            m_bindings.Add(typeof(TInterface), typeof(TImpl));
+        }
+
+        private class BindingBuilder<TInterface> : IBindingBuilder<TInterface>
+        {
+            private DivineInjector m_injector;
+
+            internal BindingBuilder(DivineInjector injector)
+            {
+                m_injector = injector;
+            }
+
+            public DivineInjector To<TImpl>()
+            {
+                m_injector.AddBinding<TInterface, TImpl>();
+                return m_injector;
+            }
+        }
+    }
+
+}
---
+++ DivingInject.Test/DivineInjectorTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TestFirst.Net.Extensions.NUnit;
+using TestFirst.Net.Matcher;
+using DivineInject;
+
+namespace DivingInject.Test
+{
+    [TestFixture]
+    public class DivineInjectorTest : AbstractNUnitScenarioTest
+    {
+        [Test]
+        public void BindsInterfaceToImplementationAndInstantiates()
+        {
+            DivineInjector injector;
+            IOrderService instance;
+            Scenario()
+                .Given(injector = new DivineInjector())
+                .Given(() => injector.Bind<IOrderService>().To<OrderService>())
+
+                .When(instance = injector.Get<IOrderService>())
+
+                .Then(instance, Is(AnInstance.OfType<OrderService>()))
+            ;
+        }
+    }
+
+    public interface IOrderService
+    { }
+
+    public class OrderService : IOrderService
+    { }
+}
