34573d995d76269b0863f4bbaba6d27f599d8da6
David Green <dave@activelylazy.co.uk>
2015-09-26 22:33:06 +0200

    Class generator generates basic class

---
+++ DivineInject.Test/ClassGeneratorTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TestFirst.Net.Extensions.NUnit;
+using TestFirst.Net.Matcher;
+
+namespace DivineInject.Test
+{
+    [TestFixture]
+    public class ClassGeneratorTest : AbstractNUnitScenarioTest
+    {
+        [Test]
+        public void GeneratesBasicClass()
+        {
+            ClassGenerator generator;
+            object instance;
+
+            Scenario()
+                .Given(generator = new ClassGenerator())
+
+                .When(instance = generator.Generate())
+
+                .Then(instance, Is(AnInstance.NotNull()))
+            ;
+        }
+    }
+}
---
+++ DivineInject/ClassGenerator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace DivineInject
+{
+    class ClassGenerator
+    {
+        public object Generate()
+        {
+            return CreateNewObject();
+        }
+
+        private static object CreateNewObject()
+        {
+            var myType = CompileResultType();
+            return Activator.CreateInstance(myType);
+        }
+
+        public static Type CompileResultType()
+        {
+            TypeBuilder tb = GetTypeBuilder();
+            ConstructorBuilder constructor = tb.DefineDefaultConstructor(MethodAttributes.Public | MethodAttributes.SpecialName | MethodAttributes.RTSpecialName);
+
+            //// NOTE: assuming your list contains Field objects with fields FieldName(string) and FieldType(Type)
+            //foreach (var field in yourListOfFields)
+            //    CreateProperty(tb, field.FieldName, field.FieldType);
+
+            Type objectType = tb.CreateType();
+            return objectType;
+        }
+
+        private static TypeBuilder GetTypeBuilder()
+        {
+            var typeSignature = "MyDynamicType";
+            var an = new AssemblyName(typeSignature);
+            AssemblyBuilder assemblyBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(an, AssemblyBuilderAccess.Run);
+            ModuleBuilder moduleBuilder = assemblyBuilder.DefineDynamicModule("MainModule");
+            TypeBuilder tb = moduleBuilder.DefineType(typeSignature
+                                , TypeAttributes.Public |
+                                TypeAttributes.Class |
+                                TypeAttributes.AutoClass |
+                                TypeAttributes.AnsiClass |
+                                TypeAttributes.BeforeFieldInit |
+                                TypeAttributes.AutoLayout
+                                , null);
+            return tb;
+        }
+    }
+}
