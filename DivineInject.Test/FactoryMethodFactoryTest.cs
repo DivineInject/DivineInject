89c56902b478f2e66f96999b7bad55f123b78885
David Green <dave@activelylazy.co.uk>
2015-10-07 22:48:20 +0200

    Creates factorymethod for method with one arg and matching constructor in target

--- DivineInject.Test/FactoryMethodFactoryTest.cs
+++ DivineInject.Test/FactoryMethodFactoryTest.cs
@@ -30,14 +30,52 @@ namespace DivineInject.Test
                 .Then(factoryMethod.Properties, Is(AList.NoItems<GeneratedProperty>()))
             ;
         }
+
+        [Test]
+        public void CreatesFactoryMethodForMethodWithOneArgAndMatchingConstructorInTarget()
+        {
+            FactoryMethodFactory factoryMethodFactory;
+            MethodInfo methodInfo;
+            IDivineInjector injector;
+            FactoryMethod factoryMethod;
+            Type domainObjectType;
+            ConstructorInfo expectedConstructor;
+
+            Scenario()
+                .Given(factoryMethodFactory = new FactoryMethodFactory())
+                .Given(methodInfo = typeof(IDummyFactory).GetMethod("MethodWithSinglePassedArg"))
+                .Given(injector = AMock<IDivineInjector>().Instance)
+                .Given(domainObjectType = typeof(DomainObjectWithSingleArgConstructor))
+                .Given(expectedConstructor = domainObjectType.GetConstructor(new []{typeof(string)}))
+
+                .When(factoryMethod = factoryMethodFactory.Create(methodInfo, injector, domainObjectType))
+
+                .Then(factoryMethod.Constructor, Is(AnInstance.SameAs(expectedConstructor)))
+                .Then(factoryMethod.Properties, Is(AList.NoItems<GeneratedProperty>()))
+            ;
+        }
     }
 
     internal interface IDummyFactory
     {
         string MethodWithNoArgs();
+        DomainObjectWithSingleArgConstructor MethodWithSinglePassedArg(string name);
     }
 
     internal class DomainObjectWithDefaultConstructor
     {
+    }
+
+    internal class DomainObjectWithSingleArgConstructor
+    {
+        public DomainObjectWithSingleArgConstructor()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DomainObjectWithSingleArgConstructor(string name)
+        {
+            
+        }
     }
 }
--- DivineInject/FactoryMethodFactory.cs
+++ DivineInject/FactoryMethodFactory.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
@@ -8,9 +9,23 @@ namespace DivineInject
     {
         public FactoryMethod Create(MethodInfo method, IDivineInjector injector, Type domainObjectType)
         {
-            var constructors = domainObjectType.GetConstructors();
+            var methodArgs = method.GetParameters();
+            var constructors = domainObjectType.GetConstructors()
+                .Where(cons => ConstructorHasAllMethodArgs(cons, methodArgs));
+            var constructor = constructors.Single();
 
-            return new FactoryMethod(constructors.First(), new GeneratedProperty[0]);
+            return new FactoryMethod(constructor, new GeneratedProperty[0]);
+        }
+
+        private bool ConstructorHasAllMethodArgs(ConstructorInfo cons, ParameterInfo[] methodArgs)
+        {
+            var consArgs = cons.GetParameters();
+
+            var consArgTypes = consArgs.Select(a => a.ParameterType).ToList();
+            var methodArgTypes = methodArgs.Select(a => a.ParameterType).ToList();
+
+            return !consArgTypes.Except(methodArgTypes).Any() &&
+                !methodArgTypes.Except(consArgTypes).Any();
         }
     }
 }
