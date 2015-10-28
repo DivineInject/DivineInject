c5432383c57dc44b0c968872328c20308ec7b9d3
David Green <dave@activelylazy.co.uk>
2015-10-28 23:54:48 +0100

    Refactor into add method

--- DivineInject/ConstructorArgList.cs
+++ DivineInject/ConstructorArgList.cs
@@ -4,13 +4,27 @@ using System.Reflection.Emit;
 
 namespace DivineInject
 {
-    internal class ConstructorArgList
+    public interface IConstructorArgList
     {
+        
+    }
+
+    internal class ConstructorArgList : IConstructorArgList
+    {
+        private readonly TypeBuilder m_tb;
+
         public ConstructorArgList(TypeBuilder tb, params IConstructorArgDefinition[] definitions)
         {
+            m_tb = tb;
             Arguments = new List<IConstructorArg>();
-            foreach (var defn in definitions.Where(defn => defn.FindExisting(Arguments) == null))
-                Arguments.Add(defn.Define(tb));
+            foreach (var defn in definitions)
+                Add(defn);
+        }
+
+        public void Add(IConstructorArgDefinition defn)
+        {
+            if (defn.FindExisting(Arguments) == null)
+                Arguments.Add(defn.Define(m_tb));
         }
 
         public IList<IConstructorArg> Arguments { get; private set; } 
